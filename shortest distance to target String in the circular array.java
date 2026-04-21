97916cd3c6e60e7f8cebfcf46c2e6a3ddca9be81
Meghasri <meghasankar2006@gmail.com>
2026-04-21 15:48:43 +0530

    Update shortest distance to target String in the circular array.java

--- shortest distance to target String in the circular array.java
+++ shortest distance to target String in the circular array.java
@@ -28,8 +28,6 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        
         int n = sc.nextInt();
         sc.nextLine();
 
